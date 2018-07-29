f7b7965db3fc52565083e2972a0eadd0994f29cb
nobii <fujisawa-shin@kayac.com>
2018-07-29 19:12:50 +0900

    風紀を正す

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
-<template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+.home home
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    components: {
+        HelloWorld
+    }
 };
 </script>

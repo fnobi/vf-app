592022a98ed90a9d70cc4f2fa3f6c2cca0f69aee
nobii <fujisawa-shin@kayac.com>
2018-08-05 14:37:28 +0900

    storeの置き方改修

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'firebase/database';
 
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from '@/store';
 import './registerServiceWorker';
 import firebaseConfig from '@/const/firebaseConfig';
 

d6528d8d23e171c7f7a9e4d3debfd99366c23458
nobii <fujisawa-shin@kayac.com>
2018-08-12 21:20:37 +0900

    text formを改善

--- src/components/RefTextForm.vue
+++ src/components/RefTextForm.vue
@@ -1,10 +1,14 @@
 <template lang="pug">
 form.ref-text-form(v-if="previewMode" @submit.prevent="startEditing")
-    .ref-text-form__text--preview(v-html="previewData")
+    .ref-text-form__text--preview(
+        v-html="previewData || '−'"
+        @click="startEditing"
+        :data-empty="!remoteData"
+    )
     p.ref-text-form__button
         button edit
 form.ref-text-form(v-else @submit.prevent="endEditing")
-    textarea.ref-text-form__text--edit(v-model="editingData")
+    textarea.ref-text-form__text--edit(v-model="editingData" ref="input")
     p.ref-text-form__button
         button(:disabled="!hasChange") ok
         button(@click.prevent="cancelEditing") cancel
@@ -13,21 +17,28 @@ form.ref-text-form(v-else @submit.prevent="endEditing")
 <style lang="scss" scoped>
 .ref-text-form {
     display: flex;
-    padding: 1em;
-    background-color: #eee;
     align-items: flex-start;
 }
 
 .ref-text-form__text {
-    width: 100%;
     flex-shrink: 1;
+    line-height: 1.5;
+    font-size: inherit;
+    padding: 0;
+    border: none;
+    font-family: sans-serif;
 }
 .ref-text-form__text--preview {
     @extend .ref-text-form__text;
+    &[data-empty] {
+        color: rgba(#000, 0.2);
+    }
 }
 .ref-text-form__text--edit {
     @extend .ref-text-form__text;
+    width: 100%;
     min-height: 10em;
+    background-color: rgba(#888, 0.2);
 }
 
 .ref-text-form__button {
@@ -80,6 +91,9 @@ export default {
         startEditing() {
             this.editingData = this.remoteData;
             this.previewMode = false;
+            setTimeout(() => {
+                this.$refs.input.focus();
+            });
         },
         endEditing() {
             const ref = this.firebaseRef(this.refPath);
